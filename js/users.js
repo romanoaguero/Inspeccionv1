0d895aecde3deb1de41566178689aeab486bea77
romanoaguero <126553194+romanoaguero@users.noreply.github.com>
2024-08-22 08:39:47 -0300

    nuevo commit

---
+++ js/users.js
@@ -0,0 +1,19 @@
+// users.js
+const users = [
+    {
+        username: "admin",
+        password: "123",
+        name: "Administrador"
+    },
+    {
+        username: "user1",
+        password: "pass1",
+        name: "Usuario Uno"
+    },
+    {
+        username: "Bruno123",
+        password: "123",
+        name: "Bruno Romano"
+    }
+    // Añade más usuarios aquí
+];
